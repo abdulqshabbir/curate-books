f004be374dd5b2fba68762181709f7a5fb8ca534
Abdul Shabbir <abdulqshabbir@gmail.com>
2020-10-05 20:13:29 +0800

    Now app sends a GET request to google-books-api depending on what is provided in the search field. Next, grab other valuable data from GET request and add ability to add books from search onto collection.

--- client/src/components/SearchBooks.js
+++ client/src/components/SearchBooks.js
@@ -2,17 +2,19 @@ import React, { useEffect, useState } from "react";
 
 export const SearchBooks = ({ setPage }) => {
   const [books, setBooks] = useState([]);
+  const [query, setQuery] = useState();
+  const [googleQuery, setGoogleQuery] = useState("");
+
   useEffect(() => {
     fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=harrypotter&key=AIzaSyBotwXy3y1-6O3MFLS3gef5a21mfeWhhYI`,
-
+      `https://www.googleapis.com/books/v1/volumes?q=${googleQuery}&key=AIzaSyBotwXy3y1-6O3MFLS3gef5a21mfeWhhYI`,
       { method: "GET" }
     )
       .then((res) => res.json())
       .then((res) => {
         return setBooks(res.items);
       });
-  }, []);
+  }, [googleQuery]);
   if (!books)
     return (
       <div>
@@ -20,6 +22,8 @@ export const SearchBooks = ({ setPage }) => {
         <button onClick={() => setPage("authors")}>Show Authors</button>
         <button onClick={() => setPage("create-book")}>Create Book</button>
         <button onClick={() => setPage("search-books")}>Search Books</button>
+        <input type="text" onChange={(e) => setQuery(e.target.value)}></input>
+        <button onClick={() => setGoogleQuery(query)}>Search for Books</button>
       </div>
     );
 
@@ -30,6 +34,8 @@ export const SearchBooks = ({ setPage }) => {
         <button onClick={() => setPage("authors")}>Show Authors</button>
         <button onClick={() => setPage("create-book")}>Create Book</button>
         <button onClick={() => setPage("search-books")}>Search Books</button>
+        <input type="text" onChange={(e) => setQuery(e.target.value)}></input>
+        <button onClick={() => setGoogleQuery(query)}>Search for Books</button>
         <ul>
           {books.map((b) => (
             <li key={b.id}>{b.volumeInfo.title}</li>
